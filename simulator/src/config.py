44715cc7d1fe91dbe60f368fe4ddc7d1a012bbf3
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-02 21:25:26 +0100

    feat: updated config with additional numbers

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -1,11 +1,12 @@
 RANGE = 150 # Range for all devices in meters
-TX_TIME = 0.05 # Time used for decoding an incoming tx
-LISTEN_TIME = 0.1 # Time used to listen to incoming txns
-E_THRESHOLD = 1.0 
-E_HARVEST_RATE = (0.001, 0.005) # Energy harvesting spectre
-E_TX, E_RX = 0.05, 0.02
-DELAY_RANGE = (0.01, 0.05)
+NODES = 10 # Amount of nodes in the simulation
+SIM_TIME = 100 # Simulation time in seconds
 
-NODES = 20 # Amount of nodes in the simulation
-SIM_TIME = 500_000 # Simulation time in seconds
+TX_TIME = 0.0195 # Time used for decoding an incoming tx
 TX_LOSS = 0.1 # Chance of loosing a tx
+DELAY_RANGE = (0.01, 0.05) # Tx delay range
+
+E_TX, E_RX, E_IDLE = 0.000041, 0.0000108, 0.000001 # Consumed energy in A during operations
+E_HARVEST_RATE = 0.00015 # Energy harvesting rate for all devices
+E_TRESHOLD = 0.00001 # Amount of energy needed to enable idle/listen mode
+E_MAX = 0.0001 # Maximum energy for a device
