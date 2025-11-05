be1fe34d09ac8c23ddff4ab38e38076a9bac0f82
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-05 19:18:49 +0100

    feat: added constants for clock and meeting simulations

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -1,6 +1,6 @@
 RANGE = 800 # Range for all devices in meters
-NODES = 10 # Amount of nodes in the simulation
-SIM_TIME = 6_000 # Simulation time in seconds
+NODES = 100 # Amount of nodes in the simulation
+SIM_TIME = 86_400 # Simulation time in seconds
 
 TX_TIME = 0.0195 # Time used for decoding an incoming tx
 TX_LOSS = 0.1 # Chance of loosing a tx
@@ -13,7 +13,9 @@ E_TX = 0.0594 # Energy used to transmit the tx
 E_RX = 0.03564 # Energy used to receive and decode the tx
 E_IDLE =  0.00000495 # Energy used to listen to tx
 
-CLOCK_DRIFT_RANGE = (0.001, 0.05) # Range for a clock drift
-WAKEUP_POINT_TRESHOLD = 120 # Wake up before meeting for this amount of seconds
+CLOCK_DRIFT_RANGE = (-0.05, 0.05) # Range for a clock drift
+WAKEUP_POINT_TRESHOLD = 60 # Wake up before meeting for half of this amount of seconds and go to sleep after another half
+MEETUP_INTERVAL = 120 # Amount of seconds to have meeting point at
+WAKEUP_RANGE = (200, 250) # Range for random wake up if device has no discoveries yet
 
-LIGHT_RANGE_LUX = (15, 500) # Number of lux for a device
+LIGHT_RANGE_LUX = (15, 100) # Number of lux for a device
