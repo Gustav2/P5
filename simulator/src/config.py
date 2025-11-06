ffe59910037d1a23f36134977e64cf5be11d387d
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-06 01:14:23 +0100

    feat: added daytime to a simulation

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -1,6 +1,8 @@
 RANGE = 800 # Range for all devices in meters
-NODES = 100 # Amount of nodes in the simulation
-SIM_TIME = 86_400 # Simulation time in seconds
+NODES = 50 # Amount of nodes in the simulation
+SIM_TIME = 2* 86_400 # Simulation time in seconds
+START_SYNC_AFTER = 86_400 # Time in simulation after sync starts
+SUNRISE_TIME, SUNSET_TIME = 8 * 24 * 60, 16 * 24 * 60 # Time of sunrise and sunset in seconds
 
 TX_TIME = 0.0195 # Time used for decoding an incoming tx
 TX_LOSS = 0.1 # Chance of loosing a tx
@@ -13,9 +15,9 @@ E_TX = 0.0594 # Energy used to transmit the tx
 E_RX = 0.03564 # Energy used to receive and decode the tx
 E_IDLE =  0.00000495 # Energy used to listen to tx
 
-CLOCK_DRIFT_RANGE = (-0.05, 0.05) # Range for a clock drift
+CLOCK_DRIFT_RANGE = (-3, 3) # Range for a clock drift
 WAKEUP_POINT_TRESHOLD = 60 # Wake up before meeting for half of this amount of seconds and go to sleep after another half
-MEETUP_INTERVAL = 120 # Amount of seconds to have meeting point at
-WAKEUP_RANGE = (200, 250) # Range for random wake up if device has no discoveries yet
+MEETUP_INTERVAL = 150 # Amount of seconds to have meeting point at
+WAKEUP_RANGE = (100, 150) # Range for random wake up if device has no discoveries yet
 
 LIGHT_RANGE_LUX = (15, 100) # Number of lux for a device
--- simulator/src/node/harvester.py
+++ simulator/src/node/harvester.py
@@ -1,18 +1,26 @@
 import random, math
 
-from ..config import LIGHT_RANGE_LUX, E_MAX, E_IDLE, E_TRESHOLD
+from ..config import LIGHT_RANGE_LUX, E_MAX, E_IDLE, E_TRESHOLD, SUNRISE_TIME, SUNSET_TIME
 
 class Harvester:
     def __init__(self,  initial_energy = 0):
         self.energy = initial_energy
         self.lux = random.uniform(*LIGHT_RANGE_LUX)
 
-    def harvest(self, time):
-        harvest_rate = max(0, (0.9083 * self.lux - 9.2714) / 100_000)
+    def harvest(self, time, local_time):     
+        daytime = local_time % 86400
+        if daytime < SUNRISE_TIME and daytime > SUNRISE_TIME:
+            return
+        
+        daytime_lux = self.lux * math.sin(math.pi * ((daytime - SUNRISE_TIME)/(SUNSET_TIME - SUNRISE_TIME)))
+
+        harvest_rate = max(0, (0.9083 * daytime_lux - 9.2714) / 100_000)
         self.energy = min(self.energy + harvest_rate * time, E_MAX)
 
     def discharge(self, joules: float):
         if joules > self.remaining_energy():
+            # TODO fix
+            return
             raise ValueError("Not enough energy to use")
         self.energy -= joules
 
