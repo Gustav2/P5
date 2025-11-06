1e9dd29b1480391ef43538f10020b4aa839ebd8c
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-06 12:43:21 +0100

    test: updated config with new test values

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -1,6 +1,6 @@
 RANGE = 800 # Range for all devices in meters
-NODES = 50 # Amount of nodes in the simulation
-SIM_TIME = 2* 86_400 # Simulation time in seconds
+NODES = 25 # Amount of nodes in the simulation
+SIM_TIME = 7 * 86_400 # Simulation time in seconds
 START_SYNC_AFTER = 86_400 # Time in simulation after sync starts
 SUNRISE_TIME, SUNSET_TIME = 8 * 24 * 60, 16 * 24 * 60 # Time of sunrise and sunset in seconds
 
@@ -15,9 +15,9 @@ E_TX = 0.0594 # Energy used to transmit the tx
 E_RX = 0.03564 # Energy used to receive and decode the tx
 E_IDLE =  0.00000495 # Energy used to listen to tx
 
-CLOCK_DRIFT_RANGE = (-3, 3) # Range for a clock drift
+CLOCK_DRIFT_RANGE = (-1, 1) # Range for a clock drift
 WAKEUP_POINT_TRESHOLD = 60 # Wake up before meeting for half of this amount of seconds and go to sleep after another half
-MEETUP_INTERVAL = 150 # Amount of seconds to have meeting point at
+MEETUP_INTERVAL = 250 # Amount of seconds to have meeting point at
 WAKEUP_RANGE = (100, 150) # Range for random wake up if device has no discoveries yet
 
 LIGHT_RANGE_LUX = (15, 100) # Number of lux for a device
