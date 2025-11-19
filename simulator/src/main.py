51fa8e8fbd78a9729e96e53b74e21657c68ec8c2
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-19 10:23:33 +0100

    feat: tried to add basic synchronization

--- simulator/src/main.py
+++ simulator/src/main.py
@@ -21,20 +21,21 @@ def main():
     print("Average discoveries:", avg_discoveries)
     print("Discovery success rate:", avg_discoveries / (NODES - 1) * 100, "%")
 
-    meetings_list = [
-        info["meetings"]
-        for n in nodes
-        for info in n.neighbors.values()
-    ]
-    avg_meetings = mean(meetings_list)
-    print("Average meetings amounts:", avg_meetings)
-
     sync_tries = [
         n.sync_tries
         for n in nodes
     ]
     avg_syncs = mean(sync_tries)
-    print("Sync success rate:", (avg_meetings / avg_syncs * 100) if avg_syncs > 0 else 0, "%")
+    print("Average sync tries:", avg_syncs)
+
+    acks_list = [
+        n.acks_received
+        for n in nodes
+    ]
+    avg_acks = mean(acks_list)
+    print("Average acks received:", avg_acks)
+
+    print("Sync success rate:", avg_acks / avg_syncs * 100, "%")
 
     EnergyLogger().plot()
 
--- simulator/src/node/node.py
+++ simulator/src/node/node.py
@@ -15,9 +15,14 @@ class Node:
 
         self.harvester = Harvester()
         self.env = env
+
+        self.is_sync = False
+        self.pending_sync_with = set()
+        self.sync_tries = 0
+        self.acks_received = 0
+        self.last_beacon = 0
         
         self.neighbors: dict = {}
-        self.sync_tries = 0
         self.clock_drift = random.uniform(*CLOCK_DRIFT_RANGE)
 
         Network.register_node(self)
@@ -29,25 +34,33 @@ class Node:
 
             wakeup_in = random.uniform(*WAKEUP_RANGE)
             soonest_meet = self._soonest_meet()
+
             if soonest_meet < wakeup_in and soonest_meet > 0:
+                self.is_sync = True
                 wakeup_in = soonest_meet
+            else:
+                self.is_sync = False
+                self.pending_sync_with.clear()
 
             yield self.env.timeout(wakeup_in)
             self.harvester.harvest(wakeup_in, self.local_time())
             self.harvester.discharge_idle(wakeup_in)
+
+            if self.is_sync:
+                closest_id = min(self.neighbors.keys(),
+                                key=lambda id: self._soonest_meet(id))
+                self.pending_sync_with = {closest_id}
 
             self._update_state()
 
             EnergyLogger().log(self.id, self.env.now, self.harvester.energy)
 
             if self.harvester.energy > E_TRESHOLD:
-                # TODO use agreement instead of random waking point for discoveries and syncs
                 heard = yield self.env.process(
-                    self.listen(random.uniform(*WAKEUP_RANGE))
+                    self.listen(random.uniform(1, WAKEUP_POINT_TRESHOLD))
                 )
                 if not heard:
-                    # TODO add SYNC message under certain conditions
-                    yield self.env.process(self.transmit())
+                    yield self.env.process(self.transmit("SYNC" if self.is_sync else "BEACON"))
                     yield self.env.process(self.listen(WAKEUP_POINT_TRESHOLD))
 
     def listen(self, duration):
@@ -74,13 +87,17 @@ class Node:
 
         if msg_type == "SYNC":
             self.sync_tries += 1
+        elif msg_type == 'BEACON':
+            self.last_beacon = self.local_time();
 
-        # TODO agree on who sent and who receive
         msg = {
             'type': msg_type,
             'id': self.id,
-            'time': self.local_time() + self.clock_drift
+            'time': self.local_time() + self.clock_drift,
         }
+
+        if msg_type == "SYNC" or msg_type == "ACK":
+            print(msg_type, "Sent", "From:", self.id)
 
         Network.broadcast(self, msg)
         self._update_state()
@@ -97,16 +114,54 @@ class Node:
 
         msg_type = msg['type']
         sender = msg['id']
-        time_offset = msg['time'] - self.local_time() + self.clock_drift
+        sender_time = msg['time']
+        time_offset = sender_time - self.local_time()
 
-        self.neighbors[sender] = {
-            "offset": time_offset,
-            "last_meet": self.local_time(),
-            # TODO calc the amount of successfull syncs
-        }
+        self._update_state()
 
-        if self._soonest_meet(sender) <= SYNC_TRESHOLD and msg_type == "SYNC":
-            yield self.env.process(self.transmit("ACK"))
+        # TODO instead of ACK in BEACON, node, who sent, can just wake up around meet up interval, and hope there will be someone 
+        if msg_type == "BEACON":
+            if sender not in self.neighbors:
+                self.neighbors[sender] = {
+                    "offset": time_offset,
+                    "last_meet": self.local_time(),
+                }
+                yield self.env.process(self.transmit("BEACON"))
+            else:
+                meet_in = self._soonest_meet(sender)
+                if meet_in <= 0:
+                    self.neighbors[sender] = {
+                        "offset": time_offset,
+                        "last_meet": self.local_time(),
+                    }
+                    yield self.env.process(self.transmit("BEACON"))
+
+        elif msg_type == "SYNC":
+            print("SYNC Received:", self.id, "From:", sender)
+            if self.is_sync and sender in self.neighbors:
+                time_to_meet = self._soonest_meet(sender)
+                # ! Here is an issue, the time_to_meet is too big somehow
+                if -SYNC_TRESHOLD <= time_to_meet <= SYNC_TRESHOLD:
+                    old_last_meet = self.neighbors[sender]["last_meet"]
+
+                    self.neighbors[sender]["offset"] = time_offset
+                    self.neighbors[sender]["last_meet"] = self.local_time()
+
+                    print(sender_time)
+                    print(f"  [SYNC] Node {self.id} updated last_meet for {sender}: {old_last_meet} -> {self.local_time()}")
+
+                    yield self.env.process(self.transmit("ACK"))
+
+        elif msg_type == "ACK":
+            print("ACK Received:", self.id, "From:", sender)
+            if sender in self.pending_sync_with:
+                old_last_meet = self.neighbors[sender]["last_meet"]
+
+                self.acks_received += 1
+                self.pending_sync_with.remove(sender)
+                self.neighbors[sender]["last_meet"] = self.local_time()
+
+                print(f"  [ACK] Node {self.id} updated last_meet for {sender}: {old_last_meet} -> {self.local_time()}")
 
         self._update_state()
 
@@ -121,19 +176,18 @@ class Node:
     def _update_state(self):
         self.state = State.Idle if self.harvester.energy > E_TRESHOLD else State.Off
 
-    def _soonest_meet(self, node_id = None):
-        meeting_point = float('inf')
-        meet_in = 0
+    def _soonest_meet(self, node_id=None):
+        meet_in = float('inf')
 
         for id, neigh in self.neighbors.items():
             adjusted_time = MEETUP_INTERVAL + neigh["offset"]
             adjusted_meeting_point = neigh["last_meet"] + adjusted_time
+            current_meet_in = adjusted_meeting_point - self.local_time()
 
-            if adjusted_meeting_point < meeting_point:
-                meeting_point = adjusted_meeting_point
-                meet_in = adjusted_time - self.local_time()
+            if node_id == id:
+                return max(0, current_meet_in)
 
-            if id == node_id:
-                return meet_in
-
+            if current_meet_in < meet_in:
+                meet_in = current_meet_in
+    
         return max(0, meet_in)
