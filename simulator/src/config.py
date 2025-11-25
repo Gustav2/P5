e80bbc9599287575e3e9478aeff6e1a8d08d4ecb
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-25 15:56:49 +0100

    feat: updated the config with new, projects based variables

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -1,12 +1,28 @@
-RANGE = 150 # Range for all devices in meters
-NODES = 10 # Amount of nodes in the simulation
-SIM_TIME = 100 # Simulation time in seconds
+ONE_DAY = 86_400 # Seconds in one day
+
+RANGE = 800 # Range for all devices in meters
+LOW_POWERED_NODES = 18 # Low powered nodes
+HIGH_POWERED_NODES = 36 # High powered nodes
+NODES = LOW_POWERED_NODES + HIGH_POWERED_NODES # Amount of nodes in the simulation
+SIM_TIME = 1 * ONE_DAY # Simulation time in seconds
+SUNRISE_TIME, SUNSET_TIME = 8 * 60 * 60, 18 * 60 * 60 # Time of sunrise and sunset in seconds
+IS_DAY_CYCLE = False # Is simulation using day cycle or charges infinitely
 
 TX_TIME = 0.0195 # Time used for decoding an incoming tx
 TX_LOSS = 0.1 # Chance of loosing a tx
 DELAY_RANGE = (0.01, 0.05) # Tx delay range
 
-E_TX, E_RX, E_IDLE = 0.000041, 0.0000108, 0.000001 # Consumed energy in A during operations
-E_HARVEST_RATE = 0.00015 # Energy harvesting rate for all devices
-E_TRESHOLD = 0.00001 # Amount of energy needed to enable idle/listen mode
-E_MAX = 0.0001 # Maximum energy for a device
+# All the energy is Joules
+E_MAX = 8.82 # Maximum used energy
+E_TRESHOLD = 1.62 # Threshold for energy capacity
+E_IDLE =  0.00000495 # Energy used in idle mode
+E_LISTEN = 0.03564 # Energy used per second to listen to tranmissions
+E_TX = 0.0594 # Energy used to transmit the tx
+E_RX = E_LISTEN * TX_TIME # Energy used to receive and decode the tx
+
+LISTEN_TIME_RANGE = (5, 10) # Range for time listening
+CLOCK_DRIFT_RANGE = (-10, 10) # Range for a clock drift
+LISTEN_TIME_RANGE = (10, 30) # Times in seconds to listen to
+
+HIGH_LIGHT_RANGE_LUX = (30, 35) # Number of lux for a high powered device
+LOW_LIGHT_RANGE_LUX = (15, 20) # Number of lux for a low powered device
