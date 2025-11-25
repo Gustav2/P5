9165e9f890477f298ae549440d1394b1793ea9dc
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-25 15:57:48 +0100

    chore: moved the logger

--- simulator/src/core/energy_logger.py
+++ simulator/src/core/energy_logger.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-from .config import E_TRESHOLD, E_MAX
+from ..config import E_TRESHOLD, E_MAX
 
 class EnergyLogger:
     enabled = True
@@ -19,20 +19,19 @@ class EnergyLogger:
     @classmethod
     def plot(cls, filename="energy_plot.png"):
         if not cls.enabled:
-            print("EnergyLogger is disabled.")
             return
 
-        plt.figure(figsize=(15, 10))
+        plt.figure(figsize=(20, 7.5))
         for node_id, records in cls.data.items():
             times, energies = zip(*records)
             plt.plot(times, energies, label=f"Node {node_id}")
 
-        plt.axhline(y=E_TRESHOLD, color='red', linestyle='--', linewidth=1, label='Energy Threshold')   
-        plt.axhline(y=E_MAX, color='red', linestyle='--', linewidth=1, label='Energy Max')   
+        plt.axhline(y=E_TRESHOLD, color='black', linestyle='--', linewidth=1.5, label='Threshold')   
+        plt.axhline(y=E_MAX, color='black', linestyle='--', linewidth=1.5, label='Max')
 
-        plt.xlabel("Time")
-        plt.ylabel("Energy (microA)")
-        plt.title("Energy over Time for All Nodes")
+        plt.xlabel("Time (seconds)")
+        plt.ylabel("Energy (joules)")
+        plt.title("Energy during Simulation")
         plt.legend(loc='upper right', fontsize='small', ncol=2)
         plt.grid(True)
         plt.tight_layout()
