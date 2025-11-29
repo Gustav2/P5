2733c5c876ae9a5cd39dc984acafa2c4e10d37cf
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-11-29 15:22:49 +0100

    fix: energy

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -4,7 +4,7 @@ HIGH_POWERED_NODES = 36 # High powered nodes
 NODES = LOW_POWERED_NODES + HIGH_POWERED_NODES # Amount of nodes in the simulation
 
 ONE_DAY = 86_400_000 # Miliseconds in one day
-SIM_TIME = 7 * ONE_DAY # Simulation time in miliseconds
+SIM_TIME = 0.5 * ONE_DAY # Simulation time in miliseconds
 SUNRISE_TIME = 8 * 60 * 60 * 1_000 # Time of sunrise in miliseconds
 SUNSET_TIME = 18 * 60 * 60 * 1_000 # Time of sunset in miliseconds
 IS_DAY_CYCLE = False # Is simulation using day cycle or charges infinitely
@@ -16,12 +16,11 @@ DELAY_RANGE = (10, 50) # Packet delay range in miliseconds
 # All the energy is Joules or Joules/miliseconds
 E_MAX = 8.82 # Maximum used energy
 E_TRESHOLD = 1.62 # Threshold for energy capacity
-E_IDLE =  0.00000495 / 1_000 # Energy used in idle mode
-E_RECEIVE = 0.03564 / 1_000 # Energy used per second in receiveing mode
-E_TX = 0.1023 / 1_000 # Energy used to transmit the packet
+E_IDLE =  0.00000495 / 1_000 # Energy used in idle mode per millisecond
+E_RECEIVE = 0.03564 / 1_000 # Energy used per millisecond in receiveing mode
+E_TX = 0.1023 # Energy used to transmit the packet
 E_RX = E_RECEIVE * PT_TIME # Energy used to receive and decode the packet
 
-LISTEN_TIME_RANGE = (500, 1_000) # Range for time listening in milliseconds
 CLOCK_DRIFT_PER_DAY = 800 # Max clock drift per day
 CLOCK_DRIFT_MULTIPLIER_RANGE = (
     (ONE_DAY - CLOCK_DRIFT_PER_DAY) / ONE_DAY,
