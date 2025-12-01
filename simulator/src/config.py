b4ebf48965206b825e2aa10f5425c71ba2cce9e6
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-12-01 10:06:45 +0100

    chore: updated config

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -4,24 +4,25 @@ HIGH_POWERED_NODES = 36 # High powered nodes
 NODES = LOW_POWERED_NODES + HIGH_POWERED_NODES # Amount of nodes in the simulation
 
 ONE_DAY = 86_400_000 # Miliseconds in one day
-SIM_TIME = 7 * ONE_DAY # Simulation time in miliseconds
+SIM_TIME = 14 * ONE_DAY # Simulation time in miliseconds
 SUNRISE_TIME = 8 * 60 * 60 * 1_000 # Time of sunrise in miliseconds
 SUNSET_TIME = 18 * 60 * 60 * 1_000 # Time of sunset in miliseconds
 IS_DAY_CYCLE = False # Is simulation using day cycle or charges infinitely
 
-PT_TIME = 19.5 # Time in miliseconds used for decoding an incoming packet
+PT_TIME = 15.94 # Time in miliseconds used for decoding an incoming packet
 PT_LOSS = 0.1 # Chance of loosing a packet
 DELAY_RANGE = (10, 50) # Packet delay range in miliseconds
 
 # All the energy is Joules or Joules/miliseconds
 E_MAX = 8.82 # Maximum used energy
 E_TRESHOLD = 1.62 # Threshold for energy capacity
-E_IDLE =  0.00000495 / 1_000 # Energy used in idle mode per miliseconds
-E_RECEIVE = 0.03564 / 1_000 # Energy used per miliseconds in receiveing mode
-E_TX = 0.1023 # Energy used to transmit the packet
+E_IDLE =  0.00000495 / 1_000 # Energy used in idle mode per millisecond
+E_RECEIVE = 0.03564 / 1_000 # Energy used per millisecond in receiveing mode
+E_TX = 0.1023 / 1_000 * PT_TIME # Energy used to transmit the packet
 E_RX = E_RECEIVE * PT_TIME # Energy used to receive and decode the packet
 
-LISTEN_TIME_RANGE = (500, 1_000) # Range for time listening in milliseconds
+LISTEN_TIME_RANGE = (1_000, 2_000) # Range for time listening in milliseconds
+
 CLOCK_DRIFT_PER_DAY = 800 # Max clock drift per day
 CLOCK_DRIFT_MULTIPLIER_RANGE = (
     (ONE_DAY - CLOCK_DRIFT_PER_DAY) / ONE_DAY,
