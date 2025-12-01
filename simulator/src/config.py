f1c7bb9b8fb1e2f6bb1a115221a74d9f3df4c46e
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-12-01 10:03:53 +0100

    feat: updated numbers for sim

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -9,7 +9,7 @@ SUNRISE_TIME = 8 * 60 * 60 * 1_000 # Time of sunrise in miliseconds
 SUNSET_TIME = 18 * 60 * 60 * 1_000 # Time of sunset in miliseconds
 IS_DAY_CYCLE = False # Is simulation using day cycle or charges infinitely
 
-PT_TIME = 19.5 # Time in miliseconds used for decoding an incoming packet
+PT_TIME = 15.94 # Time in miliseconds used for decoding an incoming packet
 PT_LOSS = 0.1 # Chance of loosing a packet
 DELAY_RANGE = (10, 50) # Packet delay range in miliseconds
 
@@ -18,15 +18,15 @@ E_MAX = 8.82 # Maximum used energy
 E_TRESHOLD = 1.62 # Threshold for energy capacity
 E_IDLE =  0.00000495 / 1_000 # Energy used in idle mode per millisecond
 E_RECEIVE = 0.03564 / 1_000 # Energy used per millisecond in receiveing mode
-E_TX = 0.1023 # Energy used to transmit the packet
+E_TX = 0.1023 / 1_000 * PT_TIME # Energy used to transmit the packet
 E_RX = E_RECEIVE * PT_TIME # Energy used to receive and decode the packet
 
-LISTEN_TIME_RANGE = (5_000, 10_000) # Range for time listening in milliseconds
+LISTEN_TIME_RANGE = (1_000, 2_000) # Range for time listening in milliseconds
 
 SYNC_INTERVAL = 1 * ONE_DAY # How ofter nodes perform sync
 SYNC_TIME = 30_000 # For how long sync is operated
 SYNC_TIME_RANGE = (2, SYNC_TIME / 2) # For how long node listens for a sync message, before sending one
-SYNC_PREPARATION_TIME = 60 * 60 * 1_000 # For how long before a sync node starts chraging
+SYNC_PREPARATION_TIME = 45 * 60 * 1_000 # For how long before a sync node starts chraging
 
 CLOCK_DRIFT_PER_DAY = 800 # Max clock drift per day
 CLOCK_DRIFT_MULTIPLIER_RANGE = (
