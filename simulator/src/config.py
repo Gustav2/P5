312470d2aea96a67115517bcdff00307228b2588
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-12-05 01:08:56 +0100

    chore: updated PT loss

--- simulator/src/config.py
+++ simulator/src/config.py
@@ -10,7 +10,7 @@ SUNSET_TIME = 18 * 60 * 60 * 1_000 # Time of sunset in miliseconds
 IS_DAY_CYCLE = False # Is simulation using day cycle or charges infinitely
 
 PT_TIME = 14.89 # Time in miliseconds used for decoding an incoming packet
-PT_LOSS = 0.1 # Chance of loosing a packet
+PT_LOSS = 0.05 # Chance of loosing a packet
 DELAY_RANGE = (10, 50) # Packet delay range in miliseconds
 
 # All the energy is Joules or Joules/miliseconds
