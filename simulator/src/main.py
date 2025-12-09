71558615c1220aee01c530d225b5e050278a9f82
maunus <mholms23@student.aau.dk>
2025-12-09 01:12:50 +0100

    added plotter.py, updated plotter.py to account for synch, changed main.py to comply

--- simulator/src/main.py
+++ simulator/src/main.py
@@ -1,50 +1,16 @@
-import simpy, random
-from statistics import mean
-from tqdm import tqdm
-
-from .node.node import Node
 from .core.network import Network
+from .core.plotter import Plotter
 
 from .core.energy_logger import EnergyLogger
 from .core.network_topology import NetworkTopology
 
-from .config import NODES, SIM_TIME, RANGE
-
 def main():
-    env = simpy.Environment()
-    nodes = [Node(env, i, random.uniform(0,RANGE), random.uniform(0,RANGE)) for i in range(NODES)]
+    durations_to_test = list(range(1, 90))
+    number_of_cycles = 2
 
-    chunk_size = SIM_TIME // 20 
-    with tqdm(total=SIM_TIME, desc="Simulating", unit="time") as pbar:
-        for t in range(0, SIM_TIME, chunk_size):
-            env.run(until=min(t + chunk_size, SIM_TIME))
-            pbar.update(chunk_size)
-
-    kpis = [n.kpi.get_disc_kpis(n.neighbors) for n in nodes]
-
-    e_per_cycle, avg_time, avg_success = [mean(metric) for metric in zip(*kpis)]
-    success_disc_e = mean([n.kpi.get_success_disc_e() for n in nodes])
-
-    print(f"Energy usage per DISC cycle: {e_per_cycle} J")
-    print(f"Time till first DISC receive: {avg_time} sec")
-    print(f"Energy per successfull DISC cycle: {success_disc_e} J")
-    print(f"DISC success rate: {avg_success} %")
-
-    sync_tries = [
-        n.sync_tries
-        for n in nodes
-    ]
-    avg_syncs = mean(sync_tries)
-    print("Avg SYNCs sent:", avg_syncs)
-
-    acks_list = [
-        n.acks_received
-        for n in nodes
-    ]
-    avg_acks = mean(acks_list)
-    print("Avg ACKs received:", avg_acks)
-
-    print("Sync success rate:", (avg_acks / avg_syncs * 100) if avg_syncs > 0 else 0, "%")
+    plotter = Plotter()
+    results = plotter.evaluation(runs=number_of_cycles, duration_days=durations_to_test)
+    plotter.plot_results(results)
 
     EnergyLogger.plot()
     NetworkTopology(Network.nodes).save()
